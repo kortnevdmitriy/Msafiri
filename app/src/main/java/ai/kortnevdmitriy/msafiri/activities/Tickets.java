3faccbbba5fb202dd5242cb5ae1a478c1a7c5da8
Kortnev Dmitriy <kortnevdmitriy@outlook.com>
2017-11-22 18:52:01 +0300

    Complete working ViewAll code that shows all the vehicle information.

--- app/src/main/java/ai/kortnevdmitriy/msafiri/activities/Tickets.java
+++ app/src/main/java/ai/kortnevdmitriy/msafiri/activities/Tickets.java
@@ -2,11 +2,33 @@ package ai.kortnevdmitriy.msafiri.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ai.kortnevdmitriy.msafiri.R;
+import ai.kortnevdmitriy.msafiri.adapters.TicketsAdapter;
+import ai.kortnevdmitriy.msafiri.entities.VehicleDetails;
 
 public class Tickets extends AppCompatActivity {
+
+    private final String TAG = Tickets.class.getName();
+    private RecyclerView ticketsRecyclerView;
+    private TicketsAdapter ticketsAdapter;
+    private List<VehicleDetails> listOfAllTickets = new ArrayList<>();
+    private VehicleDetails vehicleDetails;
+    private FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +37,71 @@ public class Tickets extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        readAllTickets();
+    }
+
+    // Access Database to retrieve data
+    private void readAllTickets() {
+        // Access a Firebase Real Database instance from your Activity
+        db = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = db.getReference().child("vehicles");
+
+        // Read from the database
+        myRef.addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                vehicleDetails = dataSnapshot.getValue(VehicleDetails.class);
+                Log.d(TAG, "Value is: " + vehicleDetails);
+                if (vehicleDetails != null) {
+                    vehicleDetails.setKey(dataSnapshot.getKey());
+                    prepareAllTicketsData();
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                ticketsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                ticketsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                ticketsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    // This method prepares and loads data from the database.
+    private void prepareAllTicketsData() {
+        // Create a RecyclerView & find it's view by id to populate it with tickets
+        ticketsRecyclerView = findViewById(R.id.ticketsRecyclerView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        ticketsRecyclerView.setLayoutManager(mLayoutManager);
+        ticketsRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        ticketsAdapter = new TicketsAdapter(listOfAllTickets, new TicketsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(VehicleDetails item) {
+
+            }
+        });
+        ticketsRecyclerView.setAdapter(ticketsAdapter);
+        listOfAllTickets.add(vehicleDetails);
+        Log.d(TAG, vehicleDetails.getCompanyName());
+        ticketsAdapter.notifyDataSetChanged();
     }
 
 }
--- app/src/main/java/ai/kortnevdmitriy/msafiri/adapters/TicketsAdapter.java
+++ app/src/main/java/ai/kortnevdmitriy/msafiri/adapters/TicketsAdapter.java
@@ -1,39 +1,78 @@
 package ai.kortnevdmitriy.msafiri.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ai.kortnevdmitriy.msafiri.R;
+import ai.kortnevdmitriy.msafiri.entities.VehicleDetails;
 
 /**
  * Created by kortn on 11/22/2017.
  */
 
 public class TicketsAdapter extends RecyclerView.Adapter<TicketsAdapter.TicketsViewHolder> {
-
+    private List<VehicleDetails> vehicleDetails;
     private OnItemClickListener listener;
+
+    public TicketsAdapter(List<VehicleDetails> vehicleDetails, OnItemClickListener listener) {
+        this.vehicleDetails = vehicleDetails;
+        this.listener = listener;
+    }
 
     @Override
     public TicketsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.tickets_list_row, parent, false);
+
+        return new TicketsAdapter.TicketsViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(TicketsViewHolder holder, int position) {
-
+        VehicleDetails detailsOfVehicles = vehicleDetails.get(position);
+        holder.companyName.setText(detailsOfVehicles.getCompanyName());
+        holder.vehicleType.setText(detailsOfVehicles.getVehicleType());
+        holder.travelRoute.setText(detailsOfVehicles.getTravelRoute());
+        holder.priceInKsh.setText(detailsOfVehicles.getPriceInKsh());
+        holder.numberPlate.setText(detailsOfVehicles.getNumberPlate());
+        holder.key = detailsOfVehicles.getKey();
+        holder.bind(detailsOfVehicles, listener);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return vehicleDetails.size();
     }
 
     public interface OnItemClickListener {
-        // TODO
+        void onItemClick(VehicleDetails item);
     }
 
     public class TicketsViewHolder extends RecyclerView.ViewHolder {
-        public TicketsViewHolder(View itemView) {
-            super(itemView);
+        TextView companyName, travelRoute, vehicleType, priceInKsh, numberPlate;
+        String key;
+
+        public TicketsViewHolder(View view) {
+            super(view);
+            companyName = view.findViewById(R.id.ticketsCompanyName);
+            vehicleType = view.findViewById(R.id.ticketsVehicleType);
+            travelRoute = view.findViewById(R.id.ticketsTravelRoute);
+            priceInKsh = view.findViewById(R.id.ticketsPriceInKsh);
+            numberPlate = view.findViewById(R.id.ticketsNumberPlate);
+        }
+
+        void bind(final VehicleDetails item, final OnItemClickListener listener) {
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(item);
+                }
+            });
         }
     }
 }
