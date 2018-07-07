81d2bbe3d5172ff640d90c6949a1f9c1b2c1064c
Kortnev Dmitriy <kortnevdmitriy@outlook.com>
2018-07-07 10:34:23 +0300

    Updated imports with androidX components imports

--- app/src/main/java/ai/kortnevdmitriy/msafiri/activities/Tickets.java
+++ app/src/main/java/ai/kortnevdmitriy/msafiri/activities/Tickets.java
@@ -2,11 +2,6 @@ package ai.kortnevdmitriy.msafiri.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,6 +17,11 @@ import java.util.List;
 import ai.kortnevdmitriy.msafiri.R;
 import ai.kortnevdmitriy.msafiri.adapters.TicketsAdapter;
 import ai.kortnevdmitriy.msafiri.entities.TicketDetails;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Tickets extends AppCompatActivity {
 
