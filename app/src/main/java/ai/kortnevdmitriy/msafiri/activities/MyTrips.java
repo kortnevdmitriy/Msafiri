598d31a0239ecb031d680ac71df8c85af8b3ff79
Kortnev Dmitriy <kortnevdmitriy@outlook.com>
2018-07-07 10:33:00 +0300

    Added My trip svg icon

--- app/src/main/java/ai/kortnevdmitriy/msafiri/activities/MyTrips.java
+++ app/src/main/java/ai/kortnevdmitriy/msafiri/activities/MyTrips.java
@@ -1,10 +1,10 @@
 package ai.kortnevdmitriy.msafiri.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import ai.kortnevdmitriy.msafiri.R;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MyTrips extends AppCompatActivity {
 
