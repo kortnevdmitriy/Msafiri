4154d669c35aafb453a55f698aa73a518293477b
Kortnev Dmitriy <kortnevdmitriy@outlook.com>
2019-01-30 18:20:21 +0300

    Converted BookedVehicles to Kotlin.

---
+++ app/src/main/java/ai/kortnevdmitriy/msafiri/models/BookedVehicles.kt
@@ -0,0 +1,287 @@
+package ai.kortnevdmitriy.msafiri.models
+
+/**
+ * Created by kortn on 11/28/2017.
+ */
+
+class BookedVehicles {
+	var key: String? = null
+	var seat1: String? = null
+	var seat2: String? = null
+	var seat3: String? = null
+	var seat4: String? = null
+	var seat5: String? = null
+	var seat6: String? = null
+	var seat7: String? = null
+	var seat8: String? = null
+	var seat9: String? = null
+	var seat10: String? = null
+	var seat11: String? = null
+	var seat12: String? = null
+	var seat13: String? = null
+	var seat14: String? = null
+	var seat15: String? = null
+	var seat16: String? = null
+	var seat17: String? = null
+	var seat18: String? = null
+	var seat19: String? = null
+	var seat20: String? = null
+	var seat21: String? = null
+	var seat22: String? = null
+	var seat23: String? = null
+	var seat24: String? = null
+	var seat25: String? = null
+	var seat26: String? = null
+	var seat27: String? = null
+	var seat28: String? = null
+	var seat29: String? = null
+	var seat30: String? = null
+	var seat31: String? = null
+	var seat32: String? = null
+	var seat33: String? = null
+	var seat34: String? = null
+	var seat35: String? = null
+	var seat36: String? = null
+	var seat37: String? = null
+	var seat38: String? = null
+	var seat39: String? = null
+	var seat40: String? = null
+	var seat41: String? = null
+	var seat42: String? = null
+	var seat43: String? = null
+	var seat44: String? = null
+	var seat45: String? = null
+	var seat46: String? = null
+	var seat47: String? = null
+	var seat48: String? = null
+	var seat49: String? = null
+	var seat50: String? = null
+	var seat51: String? = null
+	var seat52: String? = null
+	var seat53: String? = null
+	var seat54: String? = null
+	var seat55: String? = null
+	var seat56: String? = null
+	var seat57: String? = null
+	var seat58: String? = null
+	var seat59: String? = null
+	var seat60: String? = null
+	var seat61: String? = null
+	var seat62: String? = null
+	var seat63: String? = null
+	var seat64: String? = null
+	var seat65: String? = null
+	var seat66: String? = null
+	var seat67: String? = null
+	var seat68: String? = null
+	var seat69: String? = null
+	var seat70: String? = null
+	var seat71: String? = null
+	var seat72: String? = null
+	var seat73: String? = null
+	var seat74: String? = null
+	var seat75: String? = null
+	var seat76: String? = null
+	var seat77: String? = null
+	var seat78: String? = null
+	var seat79: String? = null
+	var seat80: String? = null
+	var seat81: String? = null
+	var seat82: String? = null
+	var seat83: String? = null
+	var seat84: String? = null
+	var seat85: String? = null
+	var seat86: String? = null
+	var seat87: String? = null
+	var seat88: String? = null
+	var seat89: String? = null
+	var seat90: String? = null
+	
+	constructor()
+	
+	constructor(
+		key: String,
+		seat1: String,
+		seat2: String,
+		seat3: String,
+		seat4: String,
+		seat5: String,
+		seat6: String,
+		seat7: String,
+		seat8: String,
+		seat9: String,
+		seat10: String,
+		seat11: String,
+		seat12: String,
+		seat13: String,
+		seat14: String,
+		seat15: String,
+		seat16: String,
+		seat17: String,
+		seat18: String,
+		seat19: String,
+		seat20: String,
+		seat21: String,
+		seat22: String,
+		seat23: String,
+		seat24: String,
+		seat25: String,
+		seat26: String,
+		seat27: String,
+		seat28: String,
+		seat29: String,
+		seat30: String,
+		seat31: String,
+		seat32: String,
+		seat33: String,
+		seat34: String,
+		seat35: String,
+		seat36: String,
+		seat37: String,
+		seat38: String,
+		seat39: String,
+		seat40: String,
+		seat41: String,
+		seat42: String,
+		seat43: String,
+		seat44: String,
+		seat45: String,
+		seat46: String,
+		seat47: String,
+		seat48: String,
+		seat49: String,
+		seat50: String,
+		seat51: String,
+		seat52: String,
+		seat53: String,
+		seat54: String,
+		seat55: String,
+		seat56: String,
+		seat57: String,
+		seat58: String,
+		seat59: String,
+		seat60: String,
+		seat61: String,
+		seat62: String,
+		seat63: String,
+		seat64: String,
+		seat65: String,
+		seat66: String,
+		seat67: String,
+		seat68: String,
+		seat69: String,
+		seat70: String,
+		seat71: String,
+		seat72: String,
+		seat73: String,
+		seat74: String,
+		seat75: String,
+		seat76: String,
+		seat77: String,
+		seat78: String,
+		seat79: String,
+		seat80: String,
+		seat81: String,
+		seat82: String,
+		seat83: String,
+		seat84: String,
+		seat85: String,
+		seat86: String,
+		seat87: String,
+		seat88: String,
+		seat89: String,
+		seat90: String
+	) {
+		this.key = key
+		this.seat1 = seat1
+		this.seat2 = seat2
+		this.seat3 = seat3
+		this.seat4 = seat4
+		this.seat5 = seat5
+		this.seat6 = seat6
+		this.seat7 = seat7
+		this.seat8 = seat8
+		this.seat9 = seat9
+		this.seat10 = seat10
+		this.seat11 = seat11
+		this.seat12 = seat12
+		this.seat13 = seat13
+		this.seat14 = seat14
+		this.seat15 = seat15
+		this.seat16 = seat16
+		this.seat17 = seat17
+		this.seat18 = seat18
+		this.seat19 = seat19
+		this.seat20 = seat20
+		this.seat21 = seat21
+		this.seat22 = seat22
+		this.seat23 = seat23
+		this.seat24 = seat24
+		this.seat25 = seat25
+		this.seat26 = seat26
+		this.seat27 = seat27
+		this.seat28 = seat28
+		this.seat29 = seat29
+		this.seat30 = seat30
+		this.seat31 = seat31
+		this.seat32 = seat32
+		this.seat33 = seat33
+		this.seat34 = seat34
+		this.seat35 = seat35
+		this.seat36 = seat36
+		this.seat37 = seat37
+		this.seat38 = seat38
+		this.seat39 = seat39
+		this.seat40 = seat40
+		this.seat41 = seat41
+		this.seat42 = seat42
+		this.seat43 = seat43
+		this.seat44 = seat44
+		this.seat45 = seat45
+		this.seat46 = seat46
+		this.seat47 = seat47
+		this.seat48 = seat48
+		this.seat49 = seat49
+		this.seat50 = seat50
+		this.seat51 = seat51
+		this.seat52 = seat52
+		this.seat53 = seat53
+		this.seat54 = seat54
+		this.seat55 = seat55
+		this.seat56 = seat56
+		this.seat57 = seat57
+		this.seat58 = seat58
+		this.seat59 = seat59
+		this.seat60 = seat60
+		this.seat61 = seat61
+		this.seat62 = seat62
+		this.seat63 = seat63
+		this.seat64 = seat64
+		this.seat65 = seat65
+		this.seat66 = seat66
+		this.seat67 = seat67
+		this.seat68 = seat68
+		this.seat69 = seat69
+		this.seat70 = seat70
+		this.seat71 = seat71
+		this.seat72 = seat72
+		this.seat73 = seat73
+		this.seat74 = seat74
+		this.seat75 = seat75
+		this.seat76 = seat76
+		this.seat77 = seat77
+		this.seat78 = seat78
+		this.seat79 = seat79
+		this.seat80 = seat80
+		this.seat81 = seat81
+		this.seat82 = seat82
+		this.seat83 = seat83
+		this.seat84 = seat84
+		this.seat85 = seat85
+		this.seat86 = seat86
+		this.seat87 = seat87
+		this.seat88 = seat88
+		this.seat89 = seat89
+		this.seat90 = seat90
+	}
+}
