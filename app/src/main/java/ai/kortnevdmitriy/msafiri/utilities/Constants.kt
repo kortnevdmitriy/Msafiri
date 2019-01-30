ddad9b1cc0b4ad42fc09baeb86f82b7011aa0ee2
Kortnev Dmitriy <kortnevdmitriy@outlook.com>
2019-01-30 18:41:18 +0300

    Converted Constants to Kotlin.

---
+++ app/src/main/java/ai/kortnevdmitriy/msafiri/utilities/Constants.kt
@@ -0,0 +1,23 @@
+package ai.kortnevdmitriy.msafiri.utilities
+
+/**
+ * Created by kortn on 10/12/2017.
+ */
+
+object Constants {
+	val DESTINATIONS = arrayOf(
+		"Nairobi",
+		"Mombasa",
+		"Kerugoya",
+		"Kisumu",
+		"Eldoret",
+		"Bomet",
+		"Kampala",
+		"Garissa",
+		"Mandera",
+		"Voi",
+		"Malindi",
+		"Lamu",
+		"Migori"
+	)
+}
